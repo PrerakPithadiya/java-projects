dd6cd598062bd1dc71f05d24b381160b070a74b8
Prerak Pithadiya <prerakpithadiya@gmail.com>
2024-07-18 16:03:57 +0530

    Create 5 - Password Generator.java

---
+++ 5 - Password Generator.java
@@ -0,0 +1,30 @@
+package Faculties.Kalpesh_Dave;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class RandomPasswordGenerator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt the user to enter their name
+        System.out.print("Enter your name: ");
+        String name = scanner.next();
+
+        // Generate random digits
+        Random random = new Random();
+        int randomDigits = random.nextInt(10000); // Generate a random number between 0 and 9999
+
+        // Format the random number to have 4 digits
+        String formattedDigits = String.format("%04d", randomDigits);
+
+        // Generate the random password combining name and random digits
+        String password = name + formattedDigits;
+
+        // Display the generated password
+        System.out.println("Generated Password: " + password);
+
+        scanner.close();
+    }
+}
